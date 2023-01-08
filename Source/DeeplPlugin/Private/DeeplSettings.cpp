43058cbedff7c21769ea97b6ce3aecf79da2bc50
9wind <36643825+9wind@users.noreply.github.com>
2023-01-08 15:10:51 +0100

    Revert "5.1 initial commit"

--- Source/DeeplPlugin/Private/DeeplSettings.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 W³odzimierz Iwanowski. All Rights Reserved.
-
-#include "DeeplSettings.h"
-
-UDeeplSettings::UDeeplSettings(const FObjectInitializer& ObjectInitializer)
-: Super(ObjectInitializer),
-  Endpoint("https://api-free.deepl.com/v2/translate") // by default we use end point that is specified in documentation
-{
-}
